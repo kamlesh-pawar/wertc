ead8d55f45f8892431c91041faf45d695779c788
kamlesh <kamlesh@example.com>
2021-12-21 22:44:54 +0530

    vedio

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ var http = require('http');
 var socketIO = require('socket.io');
 
 var fileServer = new(nodeStatic.Server)();
+const port = process.env.PORT || 5000;
+
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8080);
+}).listen(port);
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
